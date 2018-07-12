efeceb67670434adc0c4605f3405cce7a0f4d00c
te25son <39095798+te25son@users.noreply.github.com>
2018-07-12 08:58:24 +0200

    Create Video 1 - Dictionaries.py

---
+++ Into to Scripting in Python/Course 3 - Python Data Analysis/Week 1/Video 1 - Dictionaries.py
@@ -0,0 +1,16 @@
+"""
+Course 3
+Week 1 - Video 1 - Dictionaries
+"""
+
+# Dictionaries in Python map KEYS to Values
+# KEYS : VALUES
+
+# Anytime you have information you want to associate with a particular key,
+# a dictionary becomes a good choice
+
+dict1 = {
+    'you': 'me', 'him': 'her', 1: 'three'
+}
+
+print(dict1)
