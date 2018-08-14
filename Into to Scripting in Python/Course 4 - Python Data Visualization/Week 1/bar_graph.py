62cd51a285b76ba44e1d63ba7b67f1437eebdc43
te25son <39095798+te25son@users.noreply.github.com>
2018-08-14 14:07:55 +0200

    Add horizontal bar graph function
    also fix the data problem in original bar graph function

--- Into to Scripting in Python/Course 4 - Python Data Visualization/Week 1/bar_graph.py
+++ Into to Scripting in Python/Course 4 - Python Data Visualization/Week 1/bar_graph.py
@@ -38,8 +38,47 @@ def make_bar_graph(title, xvals, data1, data2=None, data3=None, data4=None, type
     if data2:
         bar_graph.add(data2[0], data2[1])
         
+        if data3:
+            bar_graph.add(data3[0], data3[1])
+            
+            if data4:
+                bar_graph.add(data4[0], data4[1])
+        
     # render the bar graph
     bar_graph.render_in_brower()
+    
+    
+def make_horizontal_bar(title, data1, data2=None, data3=None, data4=None)
+	"""
+    Input:
+    	title: string
+        data1: iterable where first element is string type and second element is num type
+        data2: same as data1
+        ...
+    
+    Return:
+    	returns a horizontal bar graph to the browser
+    """
+    # build the bar graph
+    bar_graph = pygal.HorizontalBar()
+    
+    # add the title
+    bar_graph.title = title
+    
+    # add the data
+    bar_graph.add(data1[0], data1[1])
+    
+    if data2:
+        bar_graph.add(data2[0], data2[1])
+        
+        if data3:
+            bar_graph.add(data3[0], data3[1])
+            
+            if data4:
+                bar_graph.add(data4[0], data4[1])
+    
+    # render the bar graph
+    bar_graph.render_in_browser()
     
     
 x_values = map(str, range(1999, 2011))
