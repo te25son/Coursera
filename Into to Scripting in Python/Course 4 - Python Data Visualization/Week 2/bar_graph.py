d8f8578273af9f59b673428e65210f0256971e63
Unknown <te25son@gmail.com>
2018-08-14 21:07:17 +0200

    Fix bugs and silly mistakes

--- Into to Scripting in Python/Course 4 - Python Data Visualization/Week 2/bar_graph.py
+++ Into to Scripting in Python/Course 4 - Python Data Visualization/Week 2/bar_graph.py
@@ -21,10 +21,10 @@ def make_bar_graph(title, xvals, data1, data2=None, data3=None, data4=None, type
     """
     # build the bar graph
     if type == 'stacked':
-        bar_graph = pygal.StackedBar()
+        bar_graph = pygal.StackedBar(height=400)
         
     else:
-        bar_graph = pygal.Bar()
+        bar_graph = pygal.Bar(height=400)
     
     # add the title
     bar_graph.title = title
@@ -33,7 +33,7 @@ def make_bar_graph(title, xvals, data1, data2=None, data3=None, data4=None, type
     bar_graph.x_labels = xvals
     
     # add the data
-    bar_graph.add(data1[0], data[1])
+    bar_graph.add(data1[0], data1[1])
     
     if data2:
         bar_graph.add(data2[0], data2[1])
@@ -45,22 +45,22 @@ def make_bar_graph(title, xvals, data1, data2=None, data3=None, data4=None, type
                 bar_graph.add(data4[0], data4[1])
         
     # render the bar graph
-    bar_graph.render_in_brower()
+    bar_graph.render_in_browser()
     
     
-def make_horizontal_bar(title, data1, data2=None, data3=None, data4=None)
-	"""
+def make_horizontal_bar(title, data1, data2=None, data3=None, data4=None):
+    """
     Input:
-    	title: string
+        title: string
         data1: iterable where first element is string type and second element is num type
         data2: same as data1
         ...
     
     Return:
-    	returns a horizontal bar graph to the browser
+        returns a horizontal bar graph to the browser
     """
     # build the bar graph
-    bar_graph = pygal.HorizontalBar()
+    bar_graph = pygal.HorizontalBar(height=400)
     
     # add the title
     bar_graph.title = title
@@ -89,5 +89,6 @@ my_other_data = ['more data', [
     random.randrange(0, 50) for x in range(12)
 ]]
 
-make_bar_graph('my first bar graph', x_values, my_data, my_other_data)
-make_bar_graph('my first bar graph', x_values, my_data, my_other_data, type='stacked')
+# make_bar_graph('my first bar graph', x_values, my_data, my_other_data)
+# make_bar_graph('my first bar graph', x_values, my_data, my_other_data, type='stacked')
+make_horizontal_bar('A Horizontal Bar Graph', my_data, my_other_data)
--- Into to Scripting in Python/Course 4 - Python Data Visualization/Week 2/radar_chart.py
+++ Into to Scripting in Python/Course 4 - Python Data Visualization/Week 2/radar_chart.py
@@ -3,37 +3,48 @@ Practice creating a radar chart
 """
 
 import pygal
+from pygal.style import LightSolarizedStyle
 
 
-def make_radar_chart(title, xvals, data):
+def make_radar_chart(title, data_title, data):
     """
     Input:
     	title: string
-        xvals: list of strings
-        data: list where first element is a string and second element a list of data
+        data_title: string
+        data: dictionary whereby the keys will be the x-axis and the values the
+        data to be plotted
         
     Output:
-    	returns a radar chart to the browser
+        returns a radar chart to the browser
     """
     # make the chart
-    radar_chart = pygal.Radar()
+    radar_chart = pygal.Radar(fill=True, style=LightSolarizedStyle)
     
     # add the title
     radar_chart.title = title
     
     # add the x-axis values
-    radar_chart.x_labels = xvals
+    radar_chart.x_labels = [item for item in data]
     
     # add the data
-    radar_chart.add(data[0], data[1])
+    radar_chart.add(
+        data_title,
+        [data[item] for item in data]
+    )
     
     #return the radar chart
     radar_chart.render_in_browser()
-    
 
-values = ['Career / Business', 'Finance', 'Spirituality', 'Personal Growth', 'Quality of Life',
-          'Fitness / Sport', 'Friends / Environment', 'Relationship']
 
-my_data = ['Tim', [10, 5, 6, 9, 7, 8, 2, 7]]
+my_dict = {
+    'Career / Business': 10,
+    'Finance': 4,
+    'Spirituality': 5,
+    'Personal Growth': 9,
+    'Quality of Life': 7,
+    'Fitness / Sport': 8,
+    'Friends / Environment': 2,
+    'Relationship': 7
+}
     
-make_radar_chart('Life Circle', values, my_data)
+make_radar_chart('Life Circle', 'Tim', my_dict)
