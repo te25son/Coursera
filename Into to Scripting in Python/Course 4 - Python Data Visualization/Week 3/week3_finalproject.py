6d2e129a81d8a7dad672700104e80324fa608b7a
Unknown <te25son@gmail.com>
2018-08-20 09:45:53 +0200

    Add function to read csv file / begin second function

--- Into to Scripting in Python/Course 4 - Python Data Visualization/Week 3/week3_finalproject.py
+++ Into to Scripting in Python/Course 4 - Python Data Visualization/Week 3/week3_finalproject.py
@@ -2,6 +2,37 @@
 Final Project for Week 3
 """
 
+import csv
+import math
+import pygal
+
+
+def read_csv_as_nested_dict(filename, keyfield, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      keyfield  - field to use as key for rows
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a dictionary of dictionaries where the outer dictionary
+      maps the value in the key_field to the corresponding row in the
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
+    """
+    table = {}
+
+    with open(filename, 'rt', newline='') as csv_file:
+
+        csv_reader = csv.DictReader(csv_file,
+                                    delimiter=separator,
+                                    quotechar=quote)
+
+        for row in csv_reader:
+            table[row[keyfield]] = row
+
+    return table
+
 
 def reconcile_countries_by_name(plot_countries, gdp_countries):
     """
@@ -26,3 +57,43 @@ def reconcile_countries_by_name(plot_countries, gdp_countries):
             master_set.add(key)
 
     return master_dict, master_set
+
+
+def build_map_dict_by_name(gdpinfo, plot_countries, year):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      plot_countries - Dictionary whose keys are plot library country codes
+                       and values are the corresponding country name
+      year           - String year to create GDP mapping for
+
+    Output:
+      A tuple containing a dictionary and two sets.  The dictionary
+      maps country codes from plot_countries to the log (base 10) of
+      the GDP value for that country in the specified year.  The first
+      set contains the country codes from plot_countries that were not
+      found in the GDP data file.  The second set contains the country
+      codes from plot_countries that were found in the GDP data file, but
+      have no GDP data for the specified year.
+    """
+    master_dict = {}
+    first_set = set()
+    second_set = set()
+
+    table = read_csv_as_nested_dict(
+        gdpinfo['gdpfile'],
+        gdpinfo['country_name'],
+        gdpinfo['separator'],
+        gdpinfo['quote']
+    )
+
+
+gdpinfo = {
+        "gdpfile": "isp_gdp.csv",
+        "separator": ",",
+        "quote": '"',
+        "min_year": 1960,
+        "max_year": 2015,
+        "country_name": "Country Name",
+        "country_code": "Country Code"
+    }
