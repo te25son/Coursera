e5775b2b42b952b9a57a5ebab32c3e44981611d0
Timothy Eason <teason@solten.cz>
2019-01-14 16:43:25 +0100

    add pointer example

---
+++ cs50/pset4/shorts/pointer2.c
@@ -0,0 +1,10 @@
+// typed and untyped pointers in c
+
+#include <stdio.h>
+
+int main()
+{
+  int* ptrA; // a typed pointer associated with the data type int
+  void* ptrB; // an untyped pointer associated with any data type
+  return 0;
+}
