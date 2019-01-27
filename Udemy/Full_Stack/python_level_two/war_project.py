1f49d27492658838c6359a95bc261fd4198b2898
Timothy <te25son@gmail.com>
2019-01-27 20:58:54 +0100

    UDEMY --oop project --fix some bugs computer still wins a lot tho

--- Udemy/Full_Stack/python_level_two/war_project.py
+++ Udemy/Full_Stack/python_level_two/war_project.py
@@ -1,9 +1,16 @@
 from random import shuffle
 import time
 
+global SUITES, RANKS, TABLE_CARDS, TOTAL_ROUNDS, WAR_COUNT
+
 SUITES = "H D S C".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 TABLE_CARDS = []
+TOTAL_ROUNDS = 0
+WAR_COUNT = 0
+
+
+
 
 
 class Deck:
@@ -19,6 +26,10 @@ class Deck:
         self.deck2 = [e for e in self.deck if self.deck.index(e) % 2 != 0]
         return (self.deck1, self.deck2)
 
+
+
+
+
 class Hand:
 
     def __init__(self, cards):
@@ -33,6 +44,10 @@ class Hand:
     def remove(self):
         return self.cards.pop()
 
+
+
+
+
 class Player:
 
     def __init__(self, name, hand):
@@ -40,15 +55,21 @@ class Player:
         self.hand = hand
 
     def play_card(self):
-        drawn_card = self.hand.remove()
-        print("{} has placed {}".format(self.name, drawn_card))
-        return drawn_card
+        if len(self.hand.cards) == 0:
+            end_game()
+        else:
+            drawn_card = self.hand.remove()
+            print("{} has placed {}".format(self.name, drawn_card))
+            return drawn_card
 
     def remove_war_cards(self):
         war_cards = []
-        if len(self.hand.cards) < 3:
-            # return self.hand.cards[:-1]  # return all cards up to but not including the last one
-            return self.hand.cards
+        if len(self.hand.cards) == 0:
+            end_game()
+        elif len(self.hand.cards) < 3:
+            for x in range(len(self.hand.cards) - 1):
+                war_cards.append(self.hand.remove())
+            return war_cards
         else:
             for x in range(3):
                 war_cards.append(self.hand.remove())
@@ -61,18 +82,54 @@ class Player:
         return len(self.hand.cards) != 0
 
 
-# def check_war(card1, card2):
-#     if card1[1] == card2[1]:
-#         WAR_COUNT += 1
-#         print("!! WAR !!")
-#         play_war(card1, card2)
-#
-# def play_war(card1, card2):
-#     TABLE_CARDS = []
-#     TABLE_CARDS.extend(user.remove_war_cards())
-#     TABLE_CARDS.extend(comp.remove_war_cards())
 
 
+
+def end_game():
+    print("\n")
+    print("GAME OVER")
+    print("\n")
+    print("Number of rounds: {}".format(str(TOTAL_ROUNDS)))
+    print("Number of wars: {}".format(str(WAR_COUNT)))
+    if user.still_has_cards():
+        print("{} won!".format(user.name))
+    else:
+        print("{} won!".format(comp.name))
+    quit()
+
+
+
+
+
+def check_war(card1, card2):
+    return card1[1] == card2[1]
+
+
+
+
+
+def play_war(card1, card2):
+    print("!! WAR !!")
+    TABLE_CARDS.extend(user.remove_war_cards())
+    TABLE_CARDS.extend(comp.remove_war_cards())
+
+    print(TABLE_CARDS)
+
+    p_card = user.play_card()
+    c_card = comp.play_card()
+
+    TABLE_CARDS.append(p_card)
+    TABLE_CARDS.append(c_card)
+
+    if check_war(p_card, c_card):
+        play_war(p_card, c_card)
+    else:
+        if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
+            print("{} wins!".format(user.name))
+            user.hand.add(TABLE_CARDS)
+        else:
+            print("{} wins!".format(comp.name))
+            comp.hand.add(TABLE_CARDS)
 
 
 
@@ -93,18 +150,18 @@ name = input("What's your name? ")
 # set user player
 user = Player(name, Hand(half2))
 
-TOTAL_ROUNDS = 0
-WAR_COUNT = 0
 
 while user.still_has_cards() and comp.still_has_cards():
     TOTAL_ROUNDS += 1
     # time.sleep(2)
     print("\n")
     print("Round {}".format(TOTAL_ROUNDS))
-    print("+" * 9)
+    print("=" * 9)
     print("Standings:")
     print("{} has {} cards".format(user.name, str(len(user.hand.cards))))
+    print(user.hand.cards)
     print("{} has {} cards".format(comp.name, str(len(comp.hand.cards))))
+    print(comp.hand.cards)
     print("Play!")
     print("\n")
     # time.sleep(2)
@@ -117,27 +174,9 @@ while user.still_has_cards() and comp.still_has_cards():
     TABLE_CARDS.append(p_card)
     TABLE_CARDS.append(c_card)
 
-    # check if war
-    if c_card[1] == p_card[1]:
+    if check_war(p_card, c_card):
         WAR_COUNT += 1
-        print("WAR!!")
-        TABLE_CARDS.extend(user.remove_war_cards())
-        TABLE_CARDS.extend(comp.remove_war_cards())
-
-        p_card = user.play_card()
-        c_card = comp.play_card()
-
-        TABLE_CARDS.append(p_card)
-        TABLE_CARDS.append(c_card)
-
-        print(TABLE_CARDS)
-
-        if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
-            print("{} wins!".format(user.name))
-            user.hand.add(TABLE_CARDS)
-        else:
-            print("{} wins!".format(comp.name))
-            comp.hand.add(TABLE_CARDS)
+        play_war(p_card, c_card)
 
     else:
         if RANKS.index(c_card[1]) < RANKS.index(p_card[1]):
@@ -147,8 +186,4 @@ while user.still_has_cards() and comp.still_has_cards():
             print("{} wins!".format(comp.name))
             comp.hand.add(TABLE_CARDS)
 
-print("\n")
-print("GAME OVER")
-print("\n")
-print("Number of rounds: {}".format(str(TOTAL_ROUNDS)))
-print("Number of wars: {}".format(str(WAR_COUNT)))
+end_game()
