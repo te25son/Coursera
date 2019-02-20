ec36cebe1ae44a302cce25959f4819448f6c366d
Timothy <te25son@gmail.com>
2019-02-20 08:15:22 +0100

    add models to models.py file and migrate changes

--- Udemy/Full_Stack/django_level_two/level_two_project/uno/models.py
+++ Udemy/Full_Stack/django_level_two/level_two_project/uno/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=200, unique=True)
+
+    # get string representation of your model
+    def __str__(self):
+        return self.top_name
+
+
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
