4f23d38ca49919d43a97cc200617f95b2c0af5e9
Timothy <te25son@gmail.com>
2019-02-21 07:45:18 +0100

    update admin file for uno app

--- Udemy/Full_Stack/django_level_two/level_two_project/uno/admin.py
+++ Udemy/Full_Stack/django_level_two/level_two_project/uno/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from uno.models import Topic, Webpage, AccessRecord
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
 
 # Register your models here.
