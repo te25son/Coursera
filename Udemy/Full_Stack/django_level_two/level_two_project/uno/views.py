85ebf0f17e8a2ebe5a814a0a58599b653174db84
Timothy <te25son@gmail.com>
2019-02-22 08:05:55 +0100

    template tagging --connect models, templates and views

--- Udemy/Full_Stack/django_level_two/level_two_project/uno/views.py
+++ Udemy/Full_Stack/django_level_two/level_two_project/uno/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from uno.models import AccessRecord, Topic, Webpage
 
 def index(request):
-    index_dict = {
-        'index_content': 'This is from the app "uno".'
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {
+        'access_records': webpages_list,
     }
-    return render(request, 'uno/index.html', context=index_dict)
+
+    return render(request, 'uno/index.html', context=date_dict)
