7c4116fd0c92e357db643d62b7fa4ab105cd8330
Timothy <te25son@gmail.com>
2019-02-27 08:04:48 +0100

    add conditions for post method to views.py

--- Udemy/Full_Stack/django_level_three/form_project/form_app/views.py
+++ Udemy/Full_Stack/django_level_three/form_project/form_app/views.py
@@ -8,4 +8,16 @@ def index(request):
 
 def forms_page(request):
     form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validation Success")
+            print(
+                'Name: ' + form.cleaned_data['name'] + '\n' +
+                'Email: ' + form.cleaned_data['email'] + '\n' +
+                'Text: ' + form.cleaned_data['text'] 
+            )
+
     return render(request, 'form_app/forms.html', {'form': form})
