4f8f6e725cd535c0022e8217edd839aabfc01e66
Timothy <te25son@gmail.com>
2019-03-03 11:14:14 +0100

    check cleaned data and compare multiple fields

--- Udemy/Full_Stack/django_level_three/form_project/form_app/forms.py
+++ Udemy/Full_Stack/django_level_three/form_project/form_app/forms.py
@@ -2,24 +2,35 @@ from django import forms
 from django.core import validators
 
 
-def check_for_z(value):
-    if value[0].lower() != 'z':
-        raise forms.ValidationError(
-            'Name must start with letter "z"'
-        )
+# def check_for_z(value):
+#     if value[0].lower() != 'z':
+#         raise forms.ValidationError(
+#             'Name must start with letter "z"'
+#         )
 
 
 class FormName(forms.Form):
 
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    verifyemail = forms.EmailField(label='Please verify your email')
     text = forms.CharField(widget=forms.Textarea)
 
     # check for bots
-    botcatcher = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput,
-        validators=[
-            validators.MaxLengthValidator(0)
-        ]
-    )
+    # botcatcher = forms.CharField(
+    #     required=False,
+    #     widget=forms.HiddenInput,
+    #     validators=[
+    #         validators.MaxLengthValidator(0)
+    #     ]
+    # )
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vemail = all_clean_data['verifyemail']
+
+        if email != vemail:
+            raise forms.ValidationError(
+                'Uh oh! The emails you entered don\'t match'
+            )
