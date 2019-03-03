bad426e2753f94183e81c38f41df3aaa50825f10
Timothy <te25son@gmail.com>
2019-03-03 11:03:17 +0100

    practice making custom validator

--- Udemy/Full_Stack/django_level_three/form_project/form_app/forms.py
+++ Udemy/Full_Stack/django_level_three/form_project/form_app/forms.py
@@ -2,9 +2,16 @@ from django import forms
 from django.core import validators
 
 
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError(
+            'Name must start with letter "z"'
+        )
+
+
 class FormName(forms.Form):
 
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
 
