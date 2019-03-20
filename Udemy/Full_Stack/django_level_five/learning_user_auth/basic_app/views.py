ce9be43cc74f9e41627e4760344c747c257bacd3
Timothy <te25son@gmail.com>
2019-03-20 08:08:11 +0100

    Set up code for registration view

--- Udemy/Full_Stack/django_level_five/learning_user_auth/basic_app/views.py
+++ Udemy/Full_Stack/django_level_five/learning_user_auth/basic_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from basic_app.forms import UserForm, UserProfileInfoForm
 
 
 def index(request):
@@ -6,4 +7,42 @@ def index(request):
 
 
 def register(request):
-    return render(request, 'basic_app/registration.html')
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user == user
+
+            if profile_pic in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+            registered = True
+
+        else:
+            print(
+                user_form.errors,
+                profile_form.errors
+            )
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(
+        request, 'basic_app/registration.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form,
+            'registered': registered
+        }
+    )
